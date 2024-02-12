7d9bf06030018d4a60d16cbab152a414c350c50f
tapiocaboy <kalanamith@outlook.com>
2024-02-12 17:07:53 +1300

    Initial commit

--- rp_poetry/main.py
+++ rp_poetry/main.py
@@ -2,16 +2,20 @@ from enum import Enum
 from fastapi import FastAPI
 import uvicorn
 
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
@@ -22,29 +26,33 @@ async def read_item(item_id: int):
 async def read_file(file_path: str):
     return {"file_path": file_path}
 
+
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name == ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    
+
     if model_name.value == "lenet":
         return {"model_name": model_name, "message": "LeCNN all the images"}
-    
+
     return {"model_name": model_name, "message": "Have some residuals"}
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+fake_items_db = [{"item_name": "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
+
 
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+    return fake_items_db[skip: skip + limit]
+
 
 @app.get("/read_item/{item_id}")
 async def read_item(item_id: str, q: str | None = None):
-    if q: 
+    if q:
         return {"item_id": item_id, "q": q}
-    
+
     return {"item_id": item_id}
+
 
 def start():
     uvicorn.run("rp_poetry.main:app", host="0.0.0.0", port=8000, reload=True)
-    
